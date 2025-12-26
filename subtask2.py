6a99ed6c5d070fa6ce572def9e0812bfaf96a0dd
Sultan Turubayev <Sultan.Turubayev@students.fh-wels.at>
2025-12-26 13:28:45 +0100

    [finishes 932] adds the code file #2

---
+++ subtask2.py
@@ -0,0 +1,24 @@
+def solve_task_8_2():
+    n = 0
+    s = 0
+    m = -1
+    a = -1
+    
+    x = int(input("Enter number (or -1 to stop): "))
+    
+    while x != -1:
+        if n == 0 or x < m:
+            m = x
+        
+        n = n + 1
+        s = s + x
+        
+        x = int(input("Enter next number: "))
+    
+    if n > 0:
+        a = s / n
+        
+    print(f"n={n}, s={s}, m={m}, a={a}")
+
+solve_task_8_2()
+
